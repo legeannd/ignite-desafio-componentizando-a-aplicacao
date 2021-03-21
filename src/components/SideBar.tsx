527ad97a8f49fb7a86001e499192fb6a09fecf23
Gean Lucas <gean.lfpsa@gmail.com>
2021-03-20 22:24:41 -0300

    Challenge completed 🎉

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
-export function SideBar() {
-  // Complete aqui
+import { useContext } from "react"
+import { SelectedCategoryContext } from "../App"
+import { Button } from "../components/Button"
+
+export function SideBar({}) {
+
+  const { genres, handleClickButton, selectedGenreId } = useContext(SelectedCategoryContext);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
